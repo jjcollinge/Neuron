722ff6132a6da185ea6540ad8a902d03666870c0
jjcollinge <jonathancollinge@live.com>
2015-04-07 14:26:17 +0100

    Substituted Strings for char arrays

--- neuron-app/src/main/resources/neuron-client/neuron-client.ino
+++ neuron-app/src/main/resources/neuron-client/neuron-client.ino
@@ -22,52 +22,53 @@ MQTT::Client<IPStack, Countdown, 100, 1> client = MQTT::Client<IPStack, Countdow
 int id;
 bool registered = false;
 //bool tempStream = false;
-//bool LEDisOn = false;
+bool LEDon = false;
 
 char PING_REQ_TOPIC_C[35];
 char PING_RES_TOPIC_C[35];
 //char CONFIG_TOPIC_C[35];
-char SENSOR_TOPIC_C[35];
+char SENSOR_REQ_TOPIC_C[35];
+char SENSOR_RES_TOPIC_C[35];
 char LED_TOPIC_C[35];
 
 void messageArrived(MQTT::MessageData& md) {
   Serial.println("Message Received");
   
   MQTT::Message &message = md.message;  
-  String payload;
-  String topic;
   
   int payloadLen = message.payloadlen;
   int topicLen = md.topicName.lenstring.len;
+  char payload[payloadLen+1];
+  char topic[topicLen+1];
   
   int x = 0;
   int y = 0;
   
   // topic first
   while(x < topicLen) {
-     topic += md.topicName.lenstring.data[x];
+     topic[x] = md.topicName.lenstring.data[x];
      x++;
   }
+  topic[topicLen] = '\0';
   // payload next
   while(y < payloadLen) {
-     payload += md.topicName.lenstring.data[x + y];
+     payload[y] = md.topicName.lenstring.data[x + y];
      y++;
   }
+  payload[payloadLen] = '\0';
   
   Serial.print("Payload: ");
   Serial.println(payload);
   Serial.print("Topic: ");
   Serial.println(topic);
   
-  if(topic.length() == 0) return; // drop any outgoing messages which are caught
+  if(topicLen == 0) return; // drop any outgoing messages which are caught
     
-  if (topic.indexOf("1234") >= 0) {
+  if (strcmp(topic, "1234") == 0) {
     Serial.println("Handling registration response");
     StaticJsonBuffer<50> jsonBuffer;
 
-    char plc[payload.length()+1];
-    payload.toCharArray(plc, payload.length()+1);
-    JsonObject& root = jsonBuffer.parseObject(plc);
+    JsonObject& root = jsonBuffer.parseObject(payload);
     const char* cid = root["payload"];
     id = atoi(cid);
     Serial.print("Set id: ");
@@ -76,13 +77,14 @@ void messageArrived(MQTT::MessageData& md) {
     sprintf(PING_REQ_TOPIC_C, "devices/%i/ping/request", id);
     sprintf(PING_RES_TOPIC_C, "devices/%i/ping/response", id);
     //sprintf(CONFIG_TOPIC_C, "devices/%i/configure", id);
-    sprintf(SENSOR_TOPIC_C, "devices/%i/sensors/0", id);
+    sprintf(SENSOR_REQ_TOPIC_C, "devices/%i/sensors/0", id);
+    sprintf(SENSOR_RES_TOPIC_C, "devices/%i/sensors/0/stream/response", id);
     sprintf(LED_TOPIC_C, "devices/%i/actuators/0", id);
 
     Serial.println("Subscribing to: ");
     Serial.println(PING_REQ_TOPIC_C);
     //Serial.println(CONFIG_TOPIC_C);
-    Serial.println(SENSOR_TOPIC_C);
+    Serial.println(SENSOR_REQ_TOPIC_C);
     Serial.println(LED_TOPIC_C);
 
     // using wildcard so don't need subscriptions
@@ -92,9 +94,9 @@ void messageArrived(MQTT::MessageData& md) {
     client.subscribe(LED_TOPIC_C, MQTT::QOS0, messageArrived);*/
     Serial.println("Registered");
     
-  } else if (topic.equals(PING_REQ_TOPIC_C)) {
+  } else if (strcmp(topic, PING_REQ_TOPIC_C) == 0) {
     Serial.println("Handling PING request");
-    char res[30];
+    char res[20];
     sprintf(res, "{'sessionId':'%i'}", id);
     MQTT::Message response;
     response.qos = MQTT::QOS0;
@@ -102,8 +104,17 @@ void messageArrived(MQTT::MessageData& md) {
     response.payload = res;
     response.payloadlen = strlen(res);
     client.publish(PING_RES_TOPIC_C, message);
+  } else if (strcmp(topic, LED_TOPIC_C) == 0) {
+    Serial.println("Handling actuator request");
+    if(LEDon) {
+      Serial.println("Turning LED off");
+      LEDon = false;
+    } else {
+      Serial.println("Turning LED on");
+      LEDon = true;
+    }
   } else {
-    Serial.println("Unsupported topic: " + topic);
+    Serial.println("Unsupported topic");
   }
 }
 
